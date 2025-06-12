4a4c14795b37d95391df66cb41de063a7f7de495
RadiKing333 <yamed0910@gmail.com>
2025-06-12 16:07:39 -0500

    Add files via upload

---
+++ Admin.jsx
@@ -0,0 +1,5 @@
+function Admin() {
+  return <h1 style={{ color: 'yellow', backgroundColor: 'black', padding: 20 }}>Panel de administración</h1>;
+}
+
+export default Admin;
---
+++ Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1 style={{ color: 'white', backgroundColor: 'black', padding: 20 }}>Bienvenido a la rifa</h1>;
+}
+
+export default Home;
